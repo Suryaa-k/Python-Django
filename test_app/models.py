5b40774dac824ce89adf08babcbab95738e25c72
Suryaa-k <suryaanish1149198@gmail.com>
2026-05-12 12:52:34 +0530

    Create Table and Update

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    branch = models.CharField(max_length=100)
+    marks = models.IntegerField()
+    college = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
